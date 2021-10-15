5cdf6858bbf4c146a5d8a01ac64a3b4986e9c783
Dan Cristian <cristian.dan@boatyardx.com>
2021-10-15 16:34:06 +0300

    practice from cap 9 css

---
+++ css-practice-cap9/shared.js
@@ -0,0 +1,53 @@
+let backdrop = document.querySelector( '.backdrop' );
+let selectedPlanButtons = document.querySelectorAll( '.plan button' );
+let modal = document.querySelector( '.modal' );
+let btnNo = document.querySelector( '#modal__btn--no' );
+let toggleButton = document.querySelector( '.toggle-button' );
+let mobileNav = document.querySelector( '.mobile-nav' )
+
+//btnNo.addEventListener( 'click', closeModal );
+
+
+for ( let i = 0; i < selectedPlanButtons.length; i++ ) {
+    selectedPlanButtons[i].addEventListener( 'click', function() {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add( 'open' );
+        backdrop.classList.add( 'open' );
+    })
+} 
+
+backdrop.addEventListener( 'click', function( )  {
+    // backdrop.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    //backdrop.classList.remove( 'open' );
+    mobileNav.classList.remove( 'open' );
+    closeModal();
+});
+if (btnNo) {
+btnNo.addEventListener( 'click', closeModal );
+}
+
+function closeModal( ) {
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if(modal) {
+        modal.classList.remove( 'open' );
+    }
+    backdrop.classList.remove( 'open' );
+}
+
+toggleButton.addEventListener( 'click', function( ) {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add( 'open' );
+    backdrop.classList.add( 'open' );
+});
+
+
+
+
+
+
+
+
